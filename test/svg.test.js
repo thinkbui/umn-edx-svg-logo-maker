61ffbbee1508c7668c4adc7cc655ba11b0dd0237
Ian Bui <thinkbui@gmail.com>
2023-04-27 21:25:41 -0500

    Fixed broken test

--- test/svg.test.js
+++ test/svg.test.js
@@ -8,7 +8,7 @@ describe('SVG', () => {
       const text = "SVG";
       const t_color = "white";
       const svgObj = new SVG(shape,color,text,t_color);
-      expect(svgObj.toString()).toEqual('<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="green" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text></svg>');
+      expect(svgObj.toString()).toEqual('<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">\n\n\t<circle cx="150" cy="100" r="80" fill="green" />\n\n\t<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>\n\n</svg>');
     });
   });
 });
